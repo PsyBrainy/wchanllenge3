b149ec69aca64fcbb66bcc1e79362163b64a04e8
PsyBrainy <martuto93@gmail.com>
2021-02-18 10:07:07 -0300

    init

---
+++ src/main/java/com/psybrainy/wchallenge3/Wchallenge3Application.java
@@ -0,0 +1,13 @@
+package com.psybrainy.wchallenge3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wchallenge3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wchallenge3Application.class, args);
+	}
+
+}
