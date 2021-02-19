95ad705d165e7d93070bdd6589da9928054dc46c
PsyBrainy <martuto93@gmail.com>
2021-02-19 11:56:14 -0300

    implementacion de ModelMapper para entity access y dto, endpoint para update de access

--- src/main/java/com/psybrainy/wchallenge3/repository/crud/AlbumCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.psybrainy.wchallenge3.repository.crud;
-
-import com.psybrainy.wchallenge3.repository.entity.AlbumEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlbumCrudRepository extends CrudRepository<AlbumEntity,Long> {
-}
